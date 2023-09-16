7d9cfe8d8c335d4c061a80ef232bd313e72bc1c6
ArunkumarVK <vkarunkumar446@gmail.com>
2023-09-17 00:37:57 +0530

    add login component

--- src/main.jsx
+++ src/main.jsx
@@ -20,7 +20,7 @@ import PrivateRoute from './components/PrivateRoute.jsx';
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
-      <Route index={true} path='/' element={<LoginScreen />} />
+      <Route index={true} path='/' element={isLoggedIn ? <Hero /> :<HomeScreen />} />
       <Route path='/login' element={<LoginScreen />} />
       <Route path='/register' element={<RegisterScreen />} />
       <Route path='' element={<PrivateRoute />}>
